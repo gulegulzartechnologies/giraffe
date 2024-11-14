969030c746452a272e6964625da4528027c4e0e6
Voila Vitamin <148288760+voilaVitamin@users.noreply.github.com>
2024-11-13 19:11:51 -0500

    fix: util test

--- utils_test.go
+++ utils_test.go
@@ -6,18 +6,21 @@ import (
 
 func TestHasher(t *testing.T) {
 	tests := []struct {
+		name     string
 		input    string
 		expected string
 	}{
-		{"hello", "5d41402abc4b2a76b9719d911017c592"},
-		{"world", "7d793037a0760186574b0282f2f435e7"},
-		{"", "d41d8cd98f00b204e9800998ecf8427e"},
+		{"Hash of 'hello'", "hello", "5d41402abc4b2a76b9719d911017c592"},
+		{"Hash of 'world'", "world", "7d793037a0760186574b0282f2f435e7"},
+		{"Hash of empty string", "", "d41d8cd98f00b204e9800998ecf8427e"},
 	}
 
 	for _, test := range tests {
-		result := Hasher(test.input)
-		if result != test.expected {
-			t.Errorf("Hasher(%q) = %q; want %q", test.input, result, test.expected)
-		}
+		t.Run(test.name, func(t *testing.T) {
+			result := Hasher(test.input)
+			if result != test.expected {
+				t.Errorf("Hasher(%q) = %q; want %q", test.input, result, test.expected)
+			}
+		})
 	}
 }
